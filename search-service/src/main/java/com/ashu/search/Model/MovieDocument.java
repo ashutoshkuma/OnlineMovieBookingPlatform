37d4d5416272d52a12e9b1e719a77921b65ebb2a
ashutoshkuma <52956667+ashutoshkuma@users.noreply.github.com>
2026-01-22 06:48:07 +0530

    Update MovieDocument.java

--- search-service/src/main/java/com/ashu/search/Model/MovieDocument.java
+++ search-service/src/main/java/com/ashu/search/Model/MovieDocument.java
@@ -1,8 +1,12 @@
+@Document(indexName = "movie_shows")
 public class MovieShowDocument {
+    @Id
     private String showId;
     private String movieId;
+    private String theatreId;
     private String theatreName;
     private String city;
+    private String language;
     private LocalDate showDate;
     private LocalTime showTime;
 }
